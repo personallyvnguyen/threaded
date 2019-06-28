23a88a74fc73c1b2635ec3eddfa588db670ba357
John <sedansvan@gmail.com>
2019-06-28 00:04:50 -0700

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # threaded
 A discord bot that sends an embed along with your response containing a preview, author info, and a link to the message you're replying to.
+
+## Instructions
+1. React with a :speech_balloon: to the original message you want to reply to. *Protip: If you type speech in the emoji picker and press enter, :speech_balloon: will be probably be chosen.*
+2. Type and send your message in the same channel to the original message.
+
+Then, the bot will ping the author an embed with your message and include a link to the original message with a little text preview.
+
+![](https://i.imgur.com/nyyOCv9.gifv)
+
+## What's the purpose of this?
+Often times there are messages you want to reply to that have been buried by new messages. When you do reply to it, no one knows what you're talking about.
+With this bot, the server members can easily see the context of the message you're replying to.
+
+## How can I test it out?
+Click on the links below to join the server. The bot is currently hosted on Heroku.
+
+## Demo Server
+https://discord.gg/awa23Ph
+
+## Bot Invite Link
+https://discordapp.com/api/oauth2/authorize?client_id=593327876246601738&permissions=76800&scope=bot
---
+++ src/index.js
@@ -0,0 +1,112 @@
+require('dotenv').config()
+const Discord = require('discord.js');
+const config = require('../config.js');
+const stripIndent = require('strip-indent');
+
+class Bot extends Discord.Client {
+  replying = {};
+  rawEvents = {
+    MESSAGE_REACTION_ADD: 'messageReactionAdd',
+    MESSAGE_REACTION_REMOVE: 'messageReactionRemove',
+  };
+
+  constructor(config) {
+    super();
+    Object.assign(this, config);
+    this.init();
+  }
+
+  ready() {
+    console.log(`Logged in as ${this.user.tag}!`);
+    this.user.setActivity(`for ${this.emoji} reactions`, { type: 3 });
+  }
+  
+  init() {
+    this.login(process.env.TOKEN);
+    this.on('message', this.message);
+    this.on('messageReactionAdd', this.messageReactionAdd);
+    this.on('messageReactionRemove', this.messageReactionRemove);
+    this.on('raw', this.raw);
+    this.on('ready', this.ready);
+    this.on('rateLimit', this.rateLimit);
+  }
+  async raw(event) {
+    if (!this.rawEvents.hasOwnProperty(event.t)) return;
+
+    const { d: data } = event;
+    const user = this.users.get(data.user_id);
+    const channel = this.channels.get(data.channel_id);
+
+    if (channel.messages.has(data.message_id)) return;
+
+    const message = await channel.fetchMessage(data.message_id);
+    const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
+    let reaction = message.reactions.get(emojiKey);
+
+    if (!reaction) {
+      const emoji = new Discord.Emoji(this.guilds.get(data.guild_id), data.emoji);
+      reaction = new Discord.MessageReaction(message, emoji, 1, data.user_id === this.user.id);
+      // See https://github.com/discordjs/guide/pull/165
+      message._addReaction(emoji, user);
+    }
+
+
+    this.emit(this.rawEvents[event.t], reaction, user);
+  }
+
+  async message(msg) {
+    if (msg.author.bot) return;
+    if (msg.channel.type !== 'text') return;
+    if (!this.replying.hasOwnProperty(msg.author.id)) return;
+
+    const pastMsg = this.replying[msg.author.id];
+    if (pastMsg.channel !== msg.channel) return;
+    
+    const embed = new Discord.RichEmbed();
+    embed.setColor(0x36393f);
+    embed.setAuthor(`${msg.member.displayName} | ${msg.author.id}`, msg.author.displayAvatarURL)
+    
+    let pastMember = pastMsg.member;
+    let pastEmbededMsg = pastMsg.content;
+    
+    if (pastMsg.author.id === this.user.id) {
+      pastMember = await this.fetchUser(pastMsg.embeds[0].author.name.match(/(?<=\| )\d+/)[0]);
+      pastEmbededMsg = pastMsg.embeds[0].description.match(/(.|\n)+(?=\n\n\*\*Original Message:\*\*)/)[0];
+    }
+    
+    embed.setDescription(stripIndent(`
+      ${msg.content}
+      
+      **Original Message:**
+      [${pastEmbededMsg.slice(0, 50)}...](${pastMsg.url})
+    `));
+    
+    msg.delete();
+    msg.channel.send(pastMember, embed);
+    delete this.replying[msg.author.id];
+  }
+
+  messageReactionAdd(rxn, user) {
+    if (user.bot) return;
+    if (rxn.emoji.name !== this.emoji) return;
+
+    this.replying[user.id] = rxn.message;
+  }
+
+  messageReactionRemove(rxn, user) {
+    if (user.bot) return;
+    if (rxn.emoji.name !== this.emoji) return;
+    if (!this.replying.hasOwnProperty(user.id)) return;
+
+    delete this.replying[user.id];
+  }
+
+  rateLimit(info) {
+    console.log(`The bot is being rate limited.`);
+    console.log(info);
+  }
+}
+
+new Bot(config);
+
+process.on('unhandledRejection', (r, p) => { console.error('Unhandled Rejection at:', p) });
